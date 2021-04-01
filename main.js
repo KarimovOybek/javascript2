e32b0d5e603799843b567efacfcc33f6112a0f01
Oybek <punchmanone15@gmail.com>
2021-04-01 14:25:15 +0500

    javascript

---
+++ main.js
@@ -0,0 +1,44 @@
+// var userAge = +prompt('Type your age')
+
+// if (userAge <= 18) {
+//     alert("Yoshsiz, O'qishingiz kerak");
+// } else if (18 < userAge && userAge <= 50){
+//     alert("Ishlashingiz kerak");
+// } else if (50 < userAge && userAge <= 59){
+//     alert("Yaqinda pensiyaga chiqasiz");
+// } else if (59 < userAge && userAge <= 150){
+//     alert("Pensionerga o'xshaysiz, hali tirik bo'lsangiz :)")
+// } else {
+//     alert("Nimadir noto'g'ri ketib qoldi")
+// }
+
+
+// var userNum1 = +prompt('Write your number')
+// var userNum2 = +prompt('Write your number')
+// var userNum3 = +prompt('Write your number')
+
+
+// if (userNum2 < userNum1 && userNum1 < userNum3 || userNum3 < userNum1 && userNum1 < userNum2){
+//     alert('Medium number ' + userNum1); 
+// } else if (userNum1 < userNum2 && userNum2 < userNum3 || userNum3 < userNum2 && userNum2 < userNum1){
+//     alert('Medium number ' + userNum2); 
+// } else if (userNum2 < userNum3 && userNum3 < userNum1 || userNum1 < userNum3 && userNum3 < userNum2){
+//     alert('Medium number ' + userNum3); 
+// } else if (userNum1 === userNum2 ){
+//     alert("You're type two same numbers");
+// } else if (userNum2 === userNum3) {
+//     alert("You're type two same numbers");
+// } else if (userNum1 === userNum3){
+//     alert("You're type two same numbers");
+// } else{
+//     alert("Type only number")
+// }
+
+
+// var userName = prompt('Type your name ')
+
+// if (userName === 'Oybek') {
+//     alert('Hello Boss')
+// } else{
+//     alert('Hello guest')
+// }
